933a18b0a2237a75a550dd92a4d0d76409a2926a
Rodwylson Lisbão <rodwylson@hotmail.com>
2018-10-04 17:46:31 -0300

    Add project files.

---
+++ ValidateConnectionString/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace ValidateConnectionString
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblMsgStatus = new System.Windows.Forms.Label();
+            this.txtDataSource = new System.Windows.Forms.TextBox();
+            this.txtDataBase = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.gpbStatus = new System.Windows.Forms.GroupBox();
+            this.btnTestar = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.gpbStatus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data Source:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DataBase:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "User:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Pass:";
+            // 
+            // lblMsgStatus
+            // 
+            this.lblMsgStatus.AutoSize = true;
+            this.lblMsgStatus.Location = new System.Drawing.Point(6, 16);
+            this.lblMsgStatus.Name = "lblMsgStatus";
+            this.lblMsgStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblMsgStatus.TabIndex = 4;
+            // 
+            // txtDataSource
+            // 
+            this.txtDataSource.Location = new System.Drawing.Point(90, 13);
+            this.txtDataSource.Name = "txtDataSource";
+            this.txtDataSource.Size = new System.Drawing.Size(230, 20);
+            this.txtDataSource.TabIndex = 5;
+            // 
+            // txtDataBase
+            // 
+            this.txtDataBase.Location = new System.Drawing.Point(90, 39);
+            this.txtDataBase.Name = "txtDataBase";
+            this.txtDataBase.Size = new System.Drawing.Size(230, 20);
+            this.txtDataBase.TabIndex = 6;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(90, 65);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(230, 20);
+            this.txtUser.TabIndex = 7;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(90, 90);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(230, 20);
+            this.txtPass.TabIndex = 8;
+            // 
+            // gpbStatus
+            // 
+            this.gpbStatus.Controls.Add(this.lblMsgStatus);
+            this.gpbStatus.Controls.Add(this.txtMsg);
+            this.gpbStatus.Location = new System.Drawing.Point(17, 126);
+            this.gpbStatus.Name = "gpbStatus";
+            this.gpbStatus.Size = new System.Drawing.Size(382, 139);
+            this.gpbStatus.TabIndex = 9;
+            this.gpbStatus.TabStop = false;
+            this.gpbStatus.Text = "Status";
+            // 
+            // btnTestar
+            // 
+            this.btnTestar.Location = new System.Drawing.Point(173, 271);
+            this.btnTestar.Name = "btnTestar";
+            this.btnTestar.Size = new System.Drawing.Size(75, 23);
+            this.btnTestar.TabIndex = 10;
+            this.btnTestar.Text = "Testar";
+            this.btnTestar.UseVisualStyleBackColor = true;
+            this.btnTestar.Click += new System.EventHandler(this.btnTestar_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(9, 36);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.ReadOnly = true;
+            this.txtMsg.Size = new System.Drawing.Size(367, 97);
+            this.txtMsg.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 306);
+            this.Controls.Add(this.btnTestar);
+            this.Controls.Add(this.gpbStatus);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtDataBase);
+            this.Controls.Add(this.txtDataSource);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gpbStatus.ResumeLayout(false);
+            this.gpbStatus.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblMsgStatus;
+        private System.Windows.Forms.TextBox txtDataSource;
+        private System.Windows.Forms.TextBox txtDataBase;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.GroupBox gpbStatus;
+        private System.Windows.Forms.Button btnTestar;
+        private System.Windows.Forms.TextBox txtMsg;
+    }
+}
+
---
+++ ValidateConnectionString/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidateConnectionString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTestar_Click(object sender, EventArgs e)
+        {
+           var dataSource = txtDataSource.Text;            
+           var initialCatalog = txtDataBase.Text;
+           var user = txtUser.Text;
+           var pass = txtPass.Text;
+
+            string connectionString = $"Password={pass}" +
+                                      $";Persist Security Info=True" +
+                                      $";User ID={user}" +
+                                      $";Initial Catalog={initialCatalog}" +
+                                      $";Data Source={dataSource}" +
+                                      $";application name=SML portal";
+           
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    lblMsgStatus.Text = "CONECTADO";
+                    lblMsgStatus.Visible = true;
+                }
+                catch (Exception ex)
+                {
+                    lblMsgStatus.Text = "Error";
+                    txtMsg.Text = $"Error: \n {ex.Message}";
+                    lblMsgStatus.Visible = true;
+                }
+               
+              /*  //
+                // The following code uses an SqlCommand based on the SqlConnection.
+                //
+                using (SqlCommand command = new SqlCommand("SELECT TOP 2 * FROM Dogs1", con))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0} {1} {2}",
+                            reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
+                    }
+                }*/
+            }
+
+
+        }
+    }
+}
